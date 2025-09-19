6fcf5e0c6b7a7c64ec5b86e702c48bf5662273aa
umfrancisco <francis.guitler@gmail.com>
2025-09-19 09:55:07 -0300

    changed profile title and added most used languages back

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -6,11 +6,11 @@ const Profile = () => {
             <div>
                 <img className={styles.avatar} src={`https://github.com/umfrancisco.png`}></img>
                 <h2 className={styles.title}>Francisco Guitler</h2>
-                <h2 className={styles.title}>Backend Developer</h2>
+                <h2 className={styles.title}>Full Stack Java Developer & Data Analyst</h2>
                 <h2 className={styles.title}><a href="https://github.com/umfrancisco">Github</a></h2>
                 <h2 className={styles.title}><a href="https://www.linkedin.com/in/francisco-guitler-1886322b4">Linkedin</a></h2>
             </div>
-            {/* <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=umfrancisco&layout=compact&langs_count=7"/> */}
+            <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=umfrancisco&layout=compact&theme=gruvbox"/>
         </header>
     )
 }
